b3b5c6d7e0b63a92d2656171894df09e69125d4f
Chris Clifford and Eric Raetz <chris@>
2017-01-23 09:16:39 -0800

    add routing for new, hot, rising, contro

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,29 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewComponent } from './new/new.component';
+import { ControversialComponent } from './controversial/controversial.component';
+import { HotComponent } from './hot/hot.component';
+import { RisingComponent } from './rising/rising.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: NewComponent
+  },
+
+  {
+    path: 'controversial',
+    component: ControversialComponent
+  },
+
+  {
+    path: 'hot',
+    component: HotComponent
+  },
+
+  {
+    path: 'rising',
+    component: RisingComponent
   }
  ];
 
